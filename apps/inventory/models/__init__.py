cd75b24a8abaf5d509bbdc217d00237900f4aea9
mnavaid925 <158593884+mnavaid925@users.noreply.github.com>
2026-08-22 19:41:16 +0500

    feat(inventory): 5.4 re-export PutawayRule + resolver in models package

--- apps/inventory/models/__init__.py
+++ apps/inventory/models/__init__.py
@@ -11,11 +11,13 @@ from .InventoryTrackingControl.StockStatuses import StockStatus
 from .PurchaseOrderManagement.ApprovalRules import PurchaseOrderApprovalRule
 from .PurchaseOrderManagement.Approvals import PurchaseOrderApproval
 from .PurchaseOrderManagement.Dispatches import PurchaseOrderDispatch
+from .ReceivingPutaway.PutawayRules import PutawayRule, resolve_putaway_suggestion
 from .VendorSupplierManagement.VendorCommunications import VendorCommunication
 from .WarehousingBinManagement.BinCapacities import BinCapacity
 from .WarehousingBinManagement.CrossDockOrders import CrossDockOrder
 
 __all__ = ["ItemAttribute", "ItemPrice", "ProductFile", "VendorCommunication",
            "PurchaseOrderApprovalRule", "PurchaseOrderApproval", "PurchaseOrderDispatch",
+           "PutawayRule", "resolve_putaway_suggestion",
            "BinCapacity", "CrossDockOrder",
            "StockStatus", "InventoryReservation"]
