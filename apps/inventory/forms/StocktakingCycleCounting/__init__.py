b8edc9729c9a9a0f0183338cc663e0b51ae8c8cf
mnavaid925 <158593884+mnavaid925@users.noreply.github.com>
2026-08-23 19:45:48 +0500

    feat(inventory): 5.11 StocktakingCycleCounting forms package init

---
+++ apps/inventory/forms/StocktakingCycleCounting/__init__.py
@@ -0,0 +1,7 @@
+from .CountPrograms import (
+    VARIANCE_STATUS_CHOICES,
+    CountProgramForm,
+    PhysicalInventoryForm,
+)
+
+__all__ = ["CountProgramForm", "PhysicalInventoryForm", "VARIANCE_STATUS_CHOICES"]
