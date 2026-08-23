1b29a76b408f98e9f200de059cbf660aea2adcf0
mnavaid925 <158593884+mnavaid925@users.noreply.github.com>
2026-08-24 04:10:51 +0500

    feat(inventory): 5.14 urls package init

---
+++ apps/inventory/urls/BarcodeRfidIntegration/__init__.py
@@ -0,0 +1,11 @@
+﻿"""Inventory BarcodeRfidIntegration URL patterns package (Sub-module 5.14)."""
+from .BarcodeLabels import urlpatterns as _br_labels
+from .RfidTags import urlpatterns as _br_tags
+from .ScanSessions import urlpatterns as _br_sessions
+
+urlpatterns = [
+    *_br_sessions,
+    *_br_tags,
+    *_br_labels,
+]
+
