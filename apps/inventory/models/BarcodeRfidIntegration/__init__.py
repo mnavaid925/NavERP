4dd3eb970b27408dfbef0f17f7ab2adc497d4261
mnavaid925 <158593884+mnavaid925@users.noreply.github.com>
2026-08-24 04:10:29 +0500

    feat(inventory): 5.14 BarcodeRfidIntegration models package init

---
+++ apps/inventory/models/BarcodeRfidIntegration/__init__.py
@@ -0,0 +1,13 @@
+﻿"""Inventory BarcodeRfidIntegration models package (Sub-module 5.14)."""
+from .BarcodeLabels import BarcodeLabel
+from .RfidTags import RfidTag
+from .ScanSessions import ScanEvent, ScanSession, resolve_code
+
+__all__ = [
+    "BarcodeLabel",
+    "ScanSession",
+    "ScanEvent",
+    "resolve_code",
+    "RfidTag",
+]
+
