6a9f111cb2b6b6c7bea98faf6dc7411cdce93b42
mnavaid925 <158593884+mnavaid925@users.noreply.github.com>
2026-08-24 04:10:39 +0500

    feat(inventory): 5.14 forms package init

---
+++ apps/inventory/forms/BarcodeRfidIntegration/__init__.py
@@ -0,0 +1,11 @@
+﻿"""Inventory BarcodeRfidIntegration forms package (Sub-module 5.14)."""
+from .BarcodeLabels import BarcodeLabelForm
+from .RfidTags import RfidTagForm
+from .ScanSessions import ScanSessionForm
+
+__all__ = [
+    "BarcodeLabelForm",
+    "ScanSessionForm",
+    "RfidTagForm",
+]
+
