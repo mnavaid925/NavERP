5d852916a40002ab18bb0410e911062f9b45a5a2
mnavaid925 <158593884+mnavaid925@users.noreply.github.com>
2026-08-23 09:14:19 +0500

    feat(inventory): 5.8 LotSerialTracking models package init (re-exports)

---
+++ apps/inventory/models/LotSerialTracking/__init__.py
@@ -0,0 +1,4 @@
+from .LotNumberRules import LotNumberRule
+from .ShelfLifePolicies import FLAG_CSS, ShelfLifePolicy, classify_lot
+
+__all__ = ["LotNumberRule", "ShelfLifePolicy", "classify_lot", "FLAG_CSS"]
