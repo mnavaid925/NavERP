5e2f8fdc628458ca73fc591251acde5956a52939
mnavaid925 <158593884+mnavaid925@users.noreply.github.com>
2026-08-25 13:23:07 +0500

    feat(inventory): 5.16 AlertsNotifications models package init

---
+++ apps/inventory/models/AlertsNotifications/__init__.py
@@ -0,0 +1,10 @@
+"""Inventory AlertsNotifications models package (Sub-module 5.16)."""
+from .AlertRules import AlertRule
+from .InventoryAlerts import InventoryAlert
+from .NotificationDeliveries import NotificationDelivery
+
+__all__ = [
+    "AlertRule",
+    "InventoryAlert",
+    "NotificationDelivery",
+]
