a036b33143dd6d805f54537c0f0e5c1a41837899
mnavaid925 <158593884+mnavaid925@users.noreply.github.com>
2026-08-26 03:24:05 +0500

    feat(procurement): 6.7 e-auction forms pkg init

---
+++ apps/procurement/forms/EAuctionManagement/__init__.py
@@ -0,0 +1,9 @@
+"""Procurement 6.7 E-Auction Management forms."""
+from .Auctions import EaucInviteForm, EauctionForm
+from .Bids import EaucBidForm
+
+__all__ = [
+    "EauctionForm",
+    "EaucInviteForm",
+    "EaucBidForm",
+]
