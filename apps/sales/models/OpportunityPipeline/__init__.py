2529ffa67df4cc4e4209631dbd929f006d5f9224
mnavaid925 <158593884+mnavaid925@users.noreply.github.com>
2026-09-24 09:52:10 +0500

    feat(sales): export opportunity pipeline models

---
+++ apps/sales/models/OpportunityPipeline/__init__.py
@@ -0,0 +1,7 @@
+from apps.sales.models.OpportunityPipeline.Pipelines import (
+    OpportunityPipelinePlacement,
+    Pipeline,
+    PipelineStage,
+)
+
+__all__ = ["OpportunityPipelinePlacement", "Pipeline", "PipelineStage"]
