728334c5caaaea98affe8618d48b071e11055d96
mnavaid925 <158593884+mnavaid925@users.noreply.github.com>
2026-09-24 10:19:19 +0500

    feat(sales): export opportunity team member actions

---
+++ apps/sales/views/OpportunityTeams/__init__.py
@@ -0,0 +1,11 @@
+from .OpportunityTeams import (
+    opportunity_team_member_add,
+    opportunity_team_member_edit,
+    opportunity_team_member_remove,
+)
+
+__all__ = [
+    "opportunity_team_member_add",
+    "opportunity_team_member_edit",
+    "opportunity_team_member_remove",
+]
