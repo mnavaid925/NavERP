c2acce91c6c41d93d59339811967ee31d32c7a56
mnavaid925 <158593884+mnavaid925@users.noreply.github.com>
2026-09-24 10:53:27 +0500

    feat(sales): export opportunity outcome views

---
+++ apps/sales/views/OpportunityOutcomes/__init__.py
@@ -0,0 +1,17 @@
+from apps.sales.views.OpportunityOutcomes.OpportunityOutcomes import (
+    opportunity_transition,
+    opportunity_win_loss_reason_create,
+    opportunity_win_loss_reason_delete,
+    opportunity_win_loss_reason_detail,
+    opportunity_win_loss_reason_edit,
+    opportunity_win_loss_reason_list,
+)
+
+__all__ = [
+    "opportunity_transition",
+    "opportunity_win_loss_reason_create",
+    "opportunity_win_loss_reason_delete",
+    "opportunity_win_loss_reason_detail",
+    "opportunity_win_loss_reason_edit",
+    "opportunity_win_loss_reason_list",
+]
