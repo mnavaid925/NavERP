c8c70200fa11870f892cac1f29706dbdfb2d6d41
mnavaid925 <158593884+mnavaid925@users.noreply.github.com>
2026-09-25 00:48:19 +0500

    feat(sales): add shared 8.1 _base helpers

---
+++ apps/sales/models/_base.py
@@ -0,0 +1,40 @@
+from django.conf import settings
+from django.db import IntegrityError, models, transaction
+
+from apps.core.utils import next_number
+
+
+class TenantEventOwned(models.Model):
+    tenant = models.ForeignKey("core.Tenant", on_delete=models.CASCADE, related_name="+", db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class TenantOwned(models.Model):
+    tenant = models.ForeignKey("core.Tenant", on_delete=models.CASCADE, related_name="+", db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class TenantNumbered(TenantOwned):
+    NUMBER_PREFIX = ""
+    number = models.CharField(max_length=20, editable=False)
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        if not self.number and self.tenant_id and self.NUMBER_PREFIX:
+            for _ in range(5):
+                self.number = next_number(type(self), self.tenant, self.NUMBER_PREFIX)
+                try:
+                    with transaction.atomic():
+                        return super().save(*args, **kwargs)
+                except IntegrityError:
+                    self.number = ""
+        return super().save(*args, **kwargs)
