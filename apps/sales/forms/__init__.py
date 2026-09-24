b34313b3ef50b1381789a3928014a64cf7fb516d
mnavaid925 <158593884+mnavaid925@users.noreply.github.com>
2026-09-25 00:48:21 +0500

    chore(sales): add 8.1 forms package exports

---
+++ apps/sales/forms/__init__.py
@@ -0,0 +1,10 @@
+from .LeadManagement.LeadScoreEvents import LeadScoreAdjustmentForm, LeadScoreCorrectionForm
+from .LeadManagement.LeadQualifications import LeadQualificationDecisionForm, LeadQualificationForm
+from .LeadManagement.LeadRoutingRules import LeadRoutingPreviewForm, LeadRoutingRuleForm, LeadRoutingRunForm
+from .LeadManagement.LeadNurtureEnrollments import LeadNurtureActivationForm, LeadNurtureEnrollmentForm, LeadNurtureExitForm
+
+__all__ = [
+    "LeadScoreAdjustmentForm", "LeadScoreCorrectionForm", "LeadQualificationDecisionForm",
+    "LeadQualificationForm", "LeadRoutingPreviewForm", "LeadRoutingRuleForm", "LeadRoutingRunForm",
+    "LeadNurtureActivationForm", "LeadNurtureEnrollmentForm", "LeadNurtureExitForm",
+]
