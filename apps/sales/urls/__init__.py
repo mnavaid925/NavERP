303679c00e11449f8055c96bb0619c39ea793f02
mnavaid925 <158593884+mnavaid925@users.noreply.github.com>
2026-09-25 06:14:13 +0500

    feat(sales): register 8.3 URL modules

--- apps/sales/urls/__init__.py
+++ apps/sales/urls/__init__.py
@@ -3,6 +3,11 @@ from .LeadManagement.LeadScoreEvents import urlpatterns as _score_events
 from .LeadManagement.LeadQualifications import urlpatterns as _qualifications
 from .LeadManagement.LeadRoutingRules import urlpatterns as _routing_rules
 from .LeadManagement.LeadNurtureEnrollments import urlpatterns as _nurture
+from .ContactAccountManagement.PartyEnrichment import urlpatterns as _enrichment
+from .ContactAccountManagement.AccountStakeholders import urlpatterns as _stakeholders
+from .ContactAccountManagement.AccountClassifications import urlpatterns as _classifications
+from .ContactAccountManagement.AccountPlans import urlpatterns as _plans
+from .ContactAccountManagement.AccountBoards import urlpatterns as _boards
 
 app_name = "sales"
 
@@ -12,4 +17,9 @@ urlpatterns = [
     *_qualifications,
     *_routing_rules,
     *_nurture,
+    *_enrichment,
+    *_stakeholders,
+    *_classifications,
+    *_plans,
+    *_boards,
 ]
