ccd62f8d58eac539286f2cbf1285ab06c203c92a
mnavaid925 <158593884+mnavaid925@users.noreply.github.com>
2026-09-25 06:14:13 +0500

    feat(sales): re-export 8.3 models

--- apps/sales/models/__init__.py
+++ apps/sales/models/__init__.py
@@ -3,8 +3,14 @@ from .LeadManagement.LeadScoreEvents import LeadScoreEvent
 from .LeadManagement.LeadQualifications import LeadQualification
 from .LeadManagement.LeadRoutingRules import LeadRoutingRule, validate_routing_conditions
 from .LeadManagement.LeadNurtureEnrollments import LeadNurtureEnrollment
+from .ContactAccountManagement.PartyEnrichment import PartyEnrichmentEvent, validate_enrichment_changes
+from .ContactAccountManagement.AccountStakeholders import AccountStakeholder
+from .ContactAccountManagement.AccountClassifications import AccountClassification
+from .ContactAccountManagement.AccountPlans import AccountPlan
 
 __all__ = [
     "TenantEventOwned", "TenantNumbered", "TenantOwned", "LeadScoreEvent",
     "LeadQualification", "LeadRoutingRule", "LeadNurtureEnrollment", "validate_routing_conditions",
+    "PartyEnrichmentEvent", "validate_enrichment_changes", "AccountStakeholder",
+    "AccountClassification", "AccountPlan",
 ]
