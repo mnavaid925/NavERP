4114f68f441e6a39118cd263aaa3f0d6db245765
mnavaid925 <158593884+mnavaid925@users.noreply.github.com>
2026-09-25 06:14:12 +0500

    feat(sales): re-export 8.3 forms

--- apps/sales/forms/__init__.py
+++ apps/sales/forms/__init__.py
@@ -2,9 +2,15 @@ from .LeadManagement.LeadScoreEvents import LeadScoreAdjustmentForm, LeadScoreCo
 from .LeadManagement.LeadQualifications import LeadQualificationDecisionForm, LeadQualificationForm
 from .LeadManagement.LeadRoutingRules import LeadRoutingPreviewForm, LeadRoutingRuleForm, LeadRoutingRunForm
 from .LeadManagement.LeadNurtureEnrollments import LeadNurtureActivationForm, LeadNurtureEnrollmentForm, LeadNurtureExitForm
+from .ContactAccountManagement.PartyEnrichment import PartyEnrichmentApplyForm, PartyEnrichmentProposalForm, PartyEnrichmentRejectForm
+from .ContactAccountManagement.AccountStakeholders import AccountStakeholderForm
+from .ContactAccountManagement.AccountClassifications import AccountClassificationForm
+from .ContactAccountManagement.AccountPlans import AccountPlanForm
 
 __all__ = [
     "LeadScoreAdjustmentForm", "LeadScoreCorrectionForm", "LeadQualificationDecisionForm",
     "LeadQualificationForm", "LeadRoutingPreviewForm", "LeadRoutingRuleForm", "LeadRoutingRunForm",
     "LeadNurtureActivationForm", "LeadNurtureEnrollmentForm", "LeadNurtureExitForm",
+    "PartyEnrichmentApplyForm", "PartyEnrichmentProposalForm", "PartyEnrichmentRejectForm",
+    "AccountStakeholderForm", "AccountClassificationForm", "AccountPlanForm",
 ]
