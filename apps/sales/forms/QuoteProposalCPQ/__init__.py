ebf5b08a54cb039db4bed83d2d9480f02c44756c
mnavaid925 <158593884+mnavaid925@users.noreply.github.com>
2026-09-26 19:44:52 +0500

    feat(sales): add QuoteProposalCPQ forms package re-exports

---
+++ apps/sales/forms/QuoteProposalCPQ/__init__.py
@@ -0,0 +1,13 @@
+from .CPQQuotes import CPQQuoteForm, CPQQuoteApprovalActionForm, CPQPortalSignForm
+from .CPQQuoteLines import CPQQuoteLineForm
+from .ProductBundles import ProductBundleOptionForm
+from .QuoteApprovalRules import QuoteApprovalRuleForm
+
+__all__ = [
+    "CPQQuoteForm",
+    "CPQQuoteApprovalActionForm",
+    "CPQPortalSignForm",
+    "CPQQuoteLineForm",
+    "ProductBundleOptionForm",
+    "QuoteApprovalRuleForm",
+]
