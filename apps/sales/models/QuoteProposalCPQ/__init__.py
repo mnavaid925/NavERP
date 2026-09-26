5c2919d02472491eb7b437dcddc4b01e6b6a1720
mnavaid925 <158593884+mnavaid925@users.noreply.github.com>
2026-09-26 19:44:30 +0500

    feat(sales): add QuoteProposalCPQ models package re-exports

---
+++ apps/sales/models/QuoteProposalCPQ/__init__.py
@@ -0,0 +1,11 @@
+from .CPQQuotes import CPQQuote
+from .CPQQuoteLines import CPQQuoteLine
+from .ProductBundles import ProductBundleOption
+from .QuoteApprovalRules import QuoteApprovalRule
+
+__all__ = [
+    "CPQQuote",
+    "CPQQuoteLine",
+    "ProductBundleOption",
+    "QuoteApprovalRule",
+]
