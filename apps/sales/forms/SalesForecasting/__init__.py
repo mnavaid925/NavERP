0ea823c4ae123bd598332f11414c0ac3b1a75445
mnavaid925 <158593884+mnavaid925@users.noreply.github.com>
2026-09-26 17:05:26 +0500

    feat(sales): 8.4 forms/SalesForecasting package re-exports

---
+++ apps/sales/forms/SalesForecasting/__init__.py
@@ -0,0 +1,23 @@
+from apps.sales.forms.SalesForecasting.ForecastPeriods import ForecastPeriodForm
+from apps.sales.forms.SalesForecasting.ForecastSubmissions import (
+    ForecastReviewForm,
+    ForecastSubmissionForm,
+)
+from apps.sales.forms.SalesForecasting.ForecastAdjustments import (
+    ForecastAdjustmentForm,
+    ForecastRevertForm,
+)
+from apps.sales.forms.SalesForecasting.ForecastScenarios import (
+    ForecastScenarioApplyForm,
+    ForecastScenarioForm,
+)
+
+__all__ = [
+    "ForecastPeriodForm",
+    "ForecastSubmissionForm",
+    "ForecastReviewForm",
+    "ForecastAdjustmentForm",
+    "ForecastRevertForm",
+    "ForecastScenarioForm",
+    "ForecastScenarioApplyForm",
+]
